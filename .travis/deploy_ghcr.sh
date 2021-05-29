552c0d8d6e99809c5b9a25c934168283dce08d6a
Cheezzhead <boris@bpvr.nl>
2021-05-29 22:53:54 +0200

    autodeploy to ghcr

---
+++ .travis/deploy_ghcr.sh
@@ -0,0 +1,8 @@
+echo "$DOCKER_PASSWORD" | docker login ghcr.io -u "$DOCKER_USERNAME" --password-stdin
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    TAG="latest"
+else
+    TAG="$TRAVIS_BRANCH"
+fi
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+docker push $TRAVIS_REPO_SLUG:$TAG

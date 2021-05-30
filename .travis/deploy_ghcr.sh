1209b970a4ab520983cbba72d1505e793a33457f
Cheezzhead <boris@bpvr.nl>
2021-05-30 12:58:50 +0200

    Fix

--- .travis/deploy_ghcr.sh
+++ .travis/deploy_ghcr.sh
@@ -1,8 +1,6 @@
+IMAGE=${TRAVIS_REPO_SLUG,,}
+TAG=latest
+
 echo "$DOCKER_PASS" | docker login ghcr.io -u "$DOCKER_USER" --password-stdin
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    TAG="latest"
-else
-    TAG="$TRAVIS_BRANCH"
-fi
-docker build -f Dockerfile -t ghcr.io/${TRAVIS_REPO_SLUG,,}:${TAG,,} .
-docker push ${TRAVIS_REPO_SLUG,,}:${TAG,,}
+docker build -f Dockerfile -t ghcr.io/${IMAGE}:${TAG}
+docker push ghcr.io/${IMAGE}:${TAG}

9177fe2cef97ca69d844a25e8f0dcc38b0a7701f
Cheezzhead <boris@bpvr.nl>
2021-05-30 13:00:17 +0200

    Proper build

--- .travis/deploy_ghcr.sh
+++ .travis/deploy_ghcr.sh
@@ -2,5 +2,5 @@ IMAGE=${TRAVIS_REPO_SLUG,,}
 TAG=latest
 
 echo "$DOCKER_PASS" | docker login ghcr.io -u "$DOCKER_USER" --password-stdin
-docker build -f Dockerfile -t ghcr.io/${IMAGE}:${TAG}
+docker build -f Dockerfile -t ghcr.io/${IMAGE}:${TAG} .
 docker push ghcr.io/${IMAGE}:${TAG}
